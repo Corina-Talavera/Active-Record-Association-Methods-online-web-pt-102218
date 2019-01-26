0e4101b3c83c3f8ce00620fcc997d9202b8571e0
Corina Talavera <corina.talavera@gmail.com>
2019-01-26 17:05:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 5) do
 
   create_table "artists", force: :cascade do |t|
